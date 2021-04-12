44da10f43d6063a5a4401f55e8db84d7f09cadcd
weaustin <weaustin@iu.edu>
2021-04-12 11:53:21 -0400

    initial file setup

---
+++ shapes.py
@@ -0,0 +1,18 @@
+import turtle
+import random
+import numpy as np
+
+def Square():
+    pass
+
+def Circle():
+    pass
+
+def Triangle():
+    pass
+
+def Rectangle():
+    pass
+
+if __name__=="__main__":
+    print("This file will establish shapes in functions, then do cool things with drawing them on the screen, either at random or by user input")
