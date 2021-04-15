ccf2194f525d81e8dcf704441b5273458a8f9368
weaustin <weaustin@iu.edu>
2021-04-15 02:16:41 -0400

    added line size and speed changes.

--- randomlines.py
+++ randomlines.py
@@ -6,6 +6,13 @@ direction = 0
 angle = 90
 t1 = turtle.Turtle()
 t1.pd()
+t1.speed(0)
+t1.color('white')
+turtle.bgcolor('black')
+t1.shape("square")
+t1.turtlesize(stretch_len=.1, stretch_wid=.1)
+t1.width(10)
+
 while True:
     distance = random.randrange(1,101)
     direction = random.randrange(1,3)
