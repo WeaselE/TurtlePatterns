38122530cc76e7e1ebf76838483c962a4ff86c06
weaustin <weaustin@iu.edu>
2021-10-01 14:31:11 -0400

    formatting changes

--- randomlines.py
+++ randomlines.py
@@ -1,13 +1,13 @@
 import turtle
 import random
- 
+
 distance = 0
 direction = 0
 angle = 90
 t1 = turtle.Turtle()
 t1.pd()
 t1.speed(0)
-t1.color('white')
+t1.color('dark green')
 turtle.bgcolor('black')
 t1.shape("square")
 t1.turtlesize(stretch_len=.1, stretch_wid=.1)
@@ -16,8 +16,8 @@ t1.width(1)
 #
 
 while True:
-    distance = random.randrange(1,101)
-    direction = random.randrange(1,3)
+    distance = random.randrange(1, 101)
+    direction = random.randrange(1, 3)
     if direction == 1:
         t1.right(angle)
         t1.forward(distance)
@@ -26,17 +26,17 @@ while True:
         t1.forward(distance)
     if t1.pos()[0] > 1000:
         t1.penup()
-        t1.setpos((0,0))
+        t1.setpos((0, 0))
         t1.pendown()
     if t1.pos()[0] < -1000:
         t1.penup()
-        t1.setpos((0,0))
+        t1.setpos((0, 0))
         t1.pendown()
     if t1.pos()[1] > 600:
         t1.penup()
-        t1.setpos((0,0))
+        t1.setpos((0, 0))
         t1.pendown()
     if t1.pos()[1] < -600:
         t1.penup()
-        t1.setpos((0,0))
+        t1.setpos((0, 0))
         t1.pendown()
